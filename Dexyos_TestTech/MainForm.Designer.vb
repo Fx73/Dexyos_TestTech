814995ccf84afa91977b36f79cc2c95d393d5b79
Fx <fx73000@yahoo.fr>
2021-03-17 01:19:13 +0100

    Ajoutez des fichiers projet.

---
+++ Dexyos_TestTech/MainForm.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.MyTextBox = New System.Windows.Forms.TextBox()
+        Me.MyDataGridView = New System.Windows.Forms.DataGridView()
+        Me.C0 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.MyDataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MyTextBox
+        '
+        Me.MyTextBox.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.MyTextBox.Location = New System.Drawing.Point(0, 250)
+        Me.MyTextBox.Name = "MyTextBox"
+        Me.MyTextBox.Size = New System.Drawing.Size(599, 23)
+        Me.MyTextBox.TabIndex = 0
+        '
+        'MyDataGridView
+        '
+        Me.MyDataGridView.AllowUserToAddRows = False
+        Me.MyDataGridView.AllowUserToDeleteRows = False
+        Me.MyDataGridView.AllowUserToResizeColumns = False
+        Me.MyDataGridView.AllowUserToResizeRows = False
+        Me.MyDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.MyDataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells
+        Me.MyDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.MyDataGridView.ColumnHeadersVisible = False
+        Me.MyDataGridView.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.C0})
+        Me.MyDataGridView.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.MyDataGridView.Location = New System.Drawing.Point(0, 0)
+        Me.MyDataGridView.Name = "MyDataGridView"
+        Me.MyDataGridView.ReadOnly = True
+        Me.MyDataGridView.RowHeadersVisible = False
+        Me.MyDataGridView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders
+        Me.MyDataGridView.RowTemplate.Height = 25
+        Me.MyDataGridView.ShowCellToolTips = False
+        Me.MyDataGridView.ShowEditingIcon = False
+        Me.MyDataGridView.Size = New System.Drawing.Size(599, 250)
+        Me.MyDataGridView.TabIndex = 1
+        '
+        'C0
+        '
+        Me.C0.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill
+        Me.C0.HeaderText = ""
+        Me.C0.MinimumWidth = 2
+        Me.C0.Name = "C0"
+        Me.C0.ReadOnly = True
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(599, 273)
+        Me.Controls.Add(Me.MyDataGridView)
+        Me.Controls.Add(Me.MyTextBox)
+        Me.Name = "MainForm"
+        Me.Text = "Dexyos : Test Technique"
+        CType(Me.MyDataGridView, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MyTextBox As TextBox
+    Friend WithEvents MyDataGridView As DataGridView
+    Friend WithEvents C0 As DataGridViewTextBoxColumn
+End Class
---
+++ Dexyos_TestTech/MainForm.vb
@@ -0,0 +1,110 @@
+﻿Public Class MainForm
+
+    Public Const LARGEUR = 6
+    Public Const HAUTEUR = 11
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For index = 1 To HAUTEUR
+            MyDataGridView.Rows.Add()
+        Next
+    End Sub
+
+
+    Private Sub MyTextBox_TextChanged(sender As Object, e As EventArgs) Handles MyTextBox.TextChanged
+        'Changement du nombre de colonne
+        Dim nbpixels = MyTextBox.Text.Length * LARGEUR + 1
+
+        While MyDataGridView.ColumnCount > nbpixels
+            MyDataGridView.Columns.RemoveAt(MyDataGridView.ColumnCount - 1)
+        End While
+        While MyDataGridView.ColumnCount < nbpixels
+            MyDataGridView.Columns.Add("", "")
+        End While
+
+
+        'Effacement de la précedante ecriture
+        For index = 1 To HAUTEUR - 1
+            MyDataGridView.Rows.RemoveAt(1)
+            MyDataGridView.Rows.Add()
+        Next
+
+        'Dessin
+        Dim col = 1
+        For Each chiffre In MyTextBox.Text
+            For Each barre In GetBarresLCD(chiffre)
+                Dessine(col, barre)
+
+            Next
+            col += LARGEUR
+        Next
+
+
+    End Sub
+
+
+    Private Sub Dessine(col, barre)
+        Select Case barre
+            Case "A"
+                MyDataGridView.Rows(1).Cells().Item(col + 1).Style.BackColor = Color.Black
+                MyDataGridView.Rows(1).Cells().Item(col + 2).Style.BackColor = Color.Black
+                MyDataGridView.Rows(1).Cells().Item(col + 3).Style.BackColor = Color.Black
+
+            Case "G"
+                MyDataGridView.Rows(5).Cells().Item(col + 1).Style.BackColor = Color.Black
+                MyDataGridView.Rows(5).Cells().Item(col + 2).Style.BackColor = Color.Black
+                MyDataGridView.Rows(5).Cells().Item(col + 3).Style.BackColor = Color.Black
+
+            Case "D"
+                MyDataGridView.Rows(9).Cells().Item(col + 1).Style.BackColor = Color.Black
+                MyDataGridView.Rows(9).Cells().Item(col + 2).Style.BackColor = Color.Black
+                MyDataGridView.Rows(9).Cells().Item(col + 3).Style.BackColor = Color.Black
+
+            Case "B"
+                MyDataGridView.Rows(2).Cells().Item(col + 4).Style.BackColor = Color.Black
+                MyDataGridView.Rows(3).Cells().Item(col + 4).Style.BackColor = Color.Black
+                MyDataGridView.Rows(4).Cells().Item(col + 4).Style.BackColor = Color.Black
+
+            Case "C"
+                MyDataGridView.Rows(6).Cells().Item(col + 4).Style.BackColor = Color.Black
+                MyDataGridView.Rows(7).Cells().Item(col + 4).Style.BackColor = Color.Black
+                MyDataGridView.Rows(8).Cells().Item(col + 4).Style.BackColor = Color.Black
+
+            Case "F"
+                MyDataGridView.Rows(2).Cells().Item(col).Style.BackColor = Color.Black
+                MyDataGridView.Rows(3).Cells().Item(col).Style.BackColor = Color.Black
+                MyDataGridView.Rows(4).Cells().Item(col).Style.BackColor = Color.Black
+
+            Case "E"
+                MyDataGridView.Rows(6).Cells().Item(col).Style.BackColor = Color.Black
+                MyDataGridView.Rows(7).Cells().Item(col).Style.BackColor = Color.Black
+                MyDataGridView.Rows(8).Cells().Item(col).Style.BackColor = Color.Black
+
+        End Select
+    End Sub
+
+
+    '    A
+    '    _
+    ' F |G|  B
+    '    - 
+    ' E |_|  C
+    '    D
+
+    Function GetBarresLCD(n) As String
+        Select Case n
+            Case "0" : Return "ABCDEF"
+            Case "1" : Return "BC"
+            Case "2" : Return "ABGED"
+            Case "3" : Return "ABCDG"
+            Case "4" : Return "FBGC"
+            Case "5" : Return "AFGCD"
+            Case "6" : Return "AFEDCG"
+            Case "7" : Return "ABC"
+            Case "8" : Return "ABCDEFG"
+            Case "9" : Return "ABCDFG"
+            Case Else : Return ""
+        End Select
+    End Function
+
+
+End Class
