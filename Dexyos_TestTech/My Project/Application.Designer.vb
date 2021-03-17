12816e8a571cd9df206a164eac31992d639da960
Fx <fx73000@yahoo.fr>
2021-03-17 02:23:42 +0100

    Add publish folder

--- Dexyos_TestTech/My Project/Application.Designer.vb
+++ Dexyos_TestTech/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Dexyos_TestTech.Form1
+            Me.MainForm = Global.Dexyos_TestTech.MainForm
         End Sub
     End Class
 End Namespace
